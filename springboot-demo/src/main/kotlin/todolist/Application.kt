c077b7e9b9a057b60cb1ab8ba8c9e656c9bd5faa
trickting <ad.special008@gmail.com>
2019-04-16 00:56:46 +0900

    springbootのApplicationクラスをリネーム

--- springboot-demo/src/main/kotlin/todolist/Application.kt
+++ springboot-demo/src/main/kotlin/todolist/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TodolistApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<TodolistApplication>(*args)
+    runApplication<Application>(*args)
 }
