11319cb88ec74d205aaf61eb4a04f856364703c9
trickting <ad.special008@gmail.com>
2019-04-17 23:43:13 +0900

    一旦Serviceクラスのインタフェース化をやめる

--- springboot-demo/src/main/kotlin/todolist/TaskService.kt
+++ springboot-demo/src/main/kotlin/todolist/TaskService.kt
@@ -2,32 +2,23 @@ package todolist
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
-
-interface TaskService {
-    fun findAll(): List<Task>
-    fun findById(id: Long): Task
-    fun create(task: Task)
-    fun deleteById(id: Long)
-}
 
 @Service
-@Transactional
-class TaskServiceImpl(@Autowired private val repository: TaskRepository) : TaskService {
+class TaskService(@Autowired private val repository: TaskRepository) {
 
-    override fun findAll(): List<Task> {
+    fun findAll(): List<Task> {
         return repository.findAll()
     }
 
-    override fun findById(id: Long): Task {
+    fun findById(id: Long): Task {
         return repository.findById(id).get()
     }
 
-    override fun create(task: Task) {
+    fun create(task: Task) {
         repository.save(task)
     }
 
-    override fun deleteById(id: Long) {
+    fun deleteById(id: Long) {
         repository.deleteById(id)
     }
 }
